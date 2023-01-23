7a7e2a810806d4e0513b6af06d9d5201112c78b1
Magdy Atef , Frontend Web Developer & MERN STACK Developer <102534938+Magdyatef339@users.noreply.github.com>
2023-01-23 09:44:28 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+var swiper = new Swiper(".home", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".coming-container", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 55000,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    568: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    968: {
+      slidesPerView: 5,
+    },
+  },
+});
